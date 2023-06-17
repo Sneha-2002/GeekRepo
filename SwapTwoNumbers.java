867b36b56efcb4d0493a8a6d56921ae34a2e59df
SNEHA KUMARI <85447147+Sneha-2002@users.noreply.github.com>
2023-06-17 12:11:53 +0530

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,19 @@
+package Introduction;
+
+public class SwapTwoNumbers {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int temp = 0;
+		
+		temp = a;
+		 a = b;
+		 b = temp;
+		 
+		 System.out.println("new a = " + a);
+		 System.out.println("new b = "+b);
+		 
+		
+	}
+
+}
